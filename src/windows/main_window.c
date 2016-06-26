2e2fdf22fcb6c3c1d86daa8f18cf5e272a99d219
andbb <anders66g@gmail.com>
2016-06-26 21:06:37 +0200

    Working wind

---
+++ src/windows/main_window.c
@@ -0,0 +1,231 @@
+#include "main_window.h"
+
+static Window *s_window;
+static Layer *s_canvas;
+
+static int s_hours, s_minutes;
+static TextLayer *s_temperature_layer;
+static TextLayer *s_city_layer;
+static BitmapLayer *s_icon_layer;
+static GBitmap *s_icon_bitmap = NULL;
+
+static AppSync s_sync;
+static uint8_t s_sync_buffer[64];
+
+enum WeatherKey {
+  WEATHER_ICON_KEY = 0x0,         // TUPLE_INT
+  WEATHER_TEMPERATURE_KEY = 0x1,  // TUPLE_CSTRING
+  WEATHER_CITY_KEY = 0x2,         // TUPLE_CSTRING
+};
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+/*    case WEATHER_ICON_KEY:
+      if (s_icon_bitmap) {
+        gbitmap_destroy(s_icon_bitmap);
+      }
+
+//      s_icon_bitmap = gbitmap_create_with_resource(WEATHER_ICONS[new_tuple->value->uint8]);
+      bitmap_layer_set_compositing_mode(s_icon_layer, GCompOpSet);
+      bitmap_layer_set_bitmap(s_icon_layer, s_icon_bitmap);
+      break;
+*/
+    case WEATHER_TEMPERATURE_KEY:
+      // App Sync keeps new_tuple in s_sync_buffer, so we may use it directly
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "WTK ");
+    text_layer_set_text(s_temperature_layer, new_tuple->value->cstring);
+      break;
+
+    case WEATHER_CITY_KEY:
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "WCK ");
+      text_layer_set_text(s_city_layer, new_tuple->value->cstring);
+      break;
+  }
+}
+
+static void request_weather(void) {
+//void request_weather(void) {
+  DictionaryIterator *iter;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "RequestWW");
+
+  AppMessageResult result =app_message_outbox_begin(&iter);
+
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "AMOB ");
+  //APP_LOG(APP_LOG_LEVEL_DEBUG,  iter);
+  
+  
+  if (!iter) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Error preparing the outbox: %d", (int)result);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "I_err");
+  
+    // Error creating outbound message
+    return;
+  }
+
+  int value = 1;
+  dict_write_int(iter, 1, &value, sizeof(int), true);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
+//static void weather_window(Window *window) {
+static void weather_window(void) {
+//    static char s_buffer[8];
+/*  Layer *window_layer = window_get_root_layer(window);
+//  GRect bounds = layer_get_bounds(window_layer);
+
+//  s_icon_layer = bitmap_layer_create(GRect(0, 10, bounds.size.w, 80));
+//  layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
+
+  s_temperature_layer = text_layer_create(GRect(0, 90, bounds.size.w, 32));
+  text_layer_set_text_color(s_temperature_layer, GColorWhite);
+  text_layer_set_background_color(s_temperature_layer, GColorClear);
+  text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
+
+  s_city_layer = text_layer_create(GRect(0, 122, bounds.size.w, 32));
+  text_layer_set_text_color(s_city_layer, GColorWhite);
+  text_layer_set_background_color(s_city_layer, GColorClear);
+  text_layer_set_font(s_city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_city_layer));
+*/
+  Tuplet initial_values[] = {
+//    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
+    TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
+  };
+
+  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer),
+      initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+  request_weather();
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Request");
+ 
+//  strftime(s_buffer, sizeof(s_buffer), "%H:%M", tick_time);
+//  APP_LOG(APP_LOG_LEVEL_INFO, "WW RW Time is now %s", s_buffer);
+//  APP_LOG(APP_LOG_LEVEL_INFO, "WW RW Time ");
+ 
+}
+
+static int32_t get_angle_for_hour(int hour) {
+  // Progress through 12 hours, out of 360 degrees
+  return (hour * 360) / 12;
+}
+
+static int32_t get_angle_for_minute(int minute) {
+  // Progress through 60 minutes, out of 360 degrees
+  return (minute * 360) / 60;
+}
+
+static void layer_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+
+  // 12 hours only, with a minimum size
+  s_hours -= (s_hours > 12) ? 12 : 0;
+
+  // Minutes are expanding circle arc
+  int minute_angle = get_angle_for_minute(s_minutes);
+  GRect frame = grect_inset(bounds, GEdgeInsets(4 * INSET));
+  graphics_context_set_fill_color(ctx, MINUTES_COLOR);
+  graphics_fill_radial(ctx, frame, GOvalScaleModeFitCircle, 20, 0, DEG_TO_TRIGANGLE(minute_angle));
+
+  // Adjust geometry variables for inner ring
+  frame = grect_inset(frame, GEdgeInsets(3 * HOURS_RADIUS));
+
+  // Hours are dots
+  for(int i = 0; i < 12; i++) {
+    int hour_angle = get_angle_for_hour(i);
+    GPoint pos = gpoint_from_polar(frame, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
+
+    graphics_context_set_fill_color(ctx, i <= s_hours ? HOURS_COLOR : HOURS_COLOR_INACTIVE);
+    graphics_fill_circle(ctx, pos, HOURS_RADIUS);
+  }
+//  request_weather();
+//  weather_window();
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  
+   request_weather();
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_canvas = layer_create(bounds);
+  layer_set_update_proc(s_canvas, layer_update_proc);
+  layer_add_child(window_layer, s_canvas);
+  s_icon_layer = bitmap_layer_create(GRect(0, 10, bounds.size.w, 80));
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_icon_layer));
+
+  s_temperature_layer = text_layer_create(GRect(0, 58, bounds.size.w, 32));
+  text_layer_set_text_color(s_temperature_layer, GColorWhite);
+  text_layer_set_background_color(s_temperature_layer, GColorClear);
+  text_layer_set_font(s_temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
+
+  s_city_layer = text_layer_create(GRect(0, 90, bounds.size.w, 32));
+  text_layer_set_text_color(s_city_layer, GColorWhite);
+  text_layer_set_background_color(s_city_layer, GColorClear);
+  text_layer_set_font(s_city_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_alignment(s_city_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_city_layer));
+  
+  
+  Tuplet initial_values[] = {
+    TupletInteger(WEATHER_ICON_KEY, (uint8_t) 1),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C"),
+    TupletCString(WEATHER_CITY_KEY, "St Pebblesburg"),
+  };
+
+  app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer),
+      initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "WLoad");
+
+ 
+}
+
+static void window_unload(Window *window) {
+  layer_destroy(s_canvas);
+  window_destroy(s_window);
+}
+
+void main_window_push() {
+  s_window = window_create();
+  window_set_background_color(s_window, BG_COLOR);
+  window_set_window_handlers(s_window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+  window_stack_push(s_window, true);
+  app_message_open(64, 64);
+}
+
+void main_window_update(int hours, int minutes) {
+/*   DictionaryIterator *iter;
+   app_message_outbox_begin(&iter);
+
+  if (!iter) {
+    // Error creating outbound message
+    return;
+  }
+
+  int value = 1;
+  dict_write_int(iter, 1, &value, sizeof(int), true);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+*/  
+  request_weather();
+  s_hours = hours;
+  s_minutes = minutes;
+  layer_mark_dirty(s_canvas);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "MWupd");
+}
